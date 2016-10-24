2d69414f3a097fcd77acb910b89a654caac17758
jake.vanorder@gmail.com <jake.vanorder@gmail.com>
2016-10-24 17:25:25 -0500

    Wrote hello world router.

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,3 +1,12 @@
 import Kitura
 
+let router = Router()
 
+router.get("/", handler: {
+    request, response, next in
+    response.send("Hello, world!")
+    next()
+})
+
+Kitura.addHTTPServer(onPort: 8090, with: router)
+Kitura.run()
