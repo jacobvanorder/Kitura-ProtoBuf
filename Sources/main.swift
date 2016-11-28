57863e19496baf3ffbfe4c61293b85d3fc31310b
jake.vanorder@gmail.com <jake.vanorder@gmail.com>
2016-11-28 17:11:28 -0600

    Added basic GET to main.swift.

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,12 @@
-print("Hello, world!")
+import Kitura
+
+let router = Router()
+
+router.get("/", handler: {
+    request, response, next in
+    response.send("Hello, world!")
+    next()
+})
+
+Kitura.addHTTPServer(onPort: 8090, with: router)
+Kitura.run()
