c745fe5b839a63dfda13bf26c34349ac9aa7f0aa
jake.vanorder@gmail.com <jake.vanorder@gmail.com>
2016-11-28 17:16:59 -0600

    Added HeliumLogger.

--- Package.swift
+++ Package.swift
@@ -4,5 +4,6 @@ let package = Package(
     name: "ProtoBuf",
     dependencies: [
     .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 2),
+    .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 1),
     ]
 )
