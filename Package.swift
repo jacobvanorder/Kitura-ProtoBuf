b3c1548200fd42b4ffdabc9721603c8419513f50
jake.vanorder@gmail.com <jake.vanorder@gmail.com>
2016-11-30 08:08:37 -0600

    Added swift-protobuf to project.

--- Package.swift
+++ Package.swift
@@ -5,5 +5,6 @@ let package = Package(
     dependencies: [
     .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 2),
     .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 1),
+    .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0,9,24))
     ]
 )
